303fc327a8055cbba7cb80b7325dfe008d5e2307
Kylar Bang <jbang4@csu.fullerton.edu>
2026-09-08 14:44:52 -0700

    second commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+bot.run(TOKEN)
